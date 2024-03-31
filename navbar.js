b4534c81628321291fdfa54820faa26e6c0fcac2
Nepmine <surajghimire13579@gmail.com>
2024-03-31 12:24:30 +0530

    The file with all the stuff_ Freshly inserted

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+function hide()
+{
+
+    document.getElementById('flash'). style. display = 'none' ;
+    
+}
+function showing(){
+    document.getElementById('flash'). style. cssText = 'display:inline-block';
+    
+}
